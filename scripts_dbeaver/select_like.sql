48aaff183b5fdcfc71d08a28588c540217046438
GabrielSerra79 <bielserra@gmail.com>
2026-03-26 18:55:03 -0300

    AUla 665

---
+++ scripts_dbeaver/select_like.sql
@@ -0,0 +1,40 @@
+SELECT * FROM users u
+
+SELECT * FROM users u
+WHERE u.first_name like '%o';
+
+SELECT * FROM users u
+WHERE u.last_name like 'm%';
+
+SELECT * FROM users u
+WHERE u.first_name like 'he%';
+
+SELECT * FROM users u
+WHERE u.first_name like 'ca%y';
+
+SELECT * FROM users u
+WHERE u.first_name like '%mo%';
+
+SELECT * FROM users u
+WHERE u.first_name like '%a%a%';
+
+SELECT * FROM users u
+WHERE u.first_name like '%a%a';
+
+SELECT * FROM users u
+WHERE u.first_name like '_a%';
+
+SELECT * FROM users u
+WHERE u.first_name like '__a%';
+
+SELECT * FROM users u
+WHERE u.first_name like '___a%';
+
+SELECT * FROM users u
+WHERE u.first_name like '_a_i%';
+
+SELECT * FROM users u
+WHERE u.first_name like '___';
+
+SELECT * FROM users u
+WHERE u.first_name like '________';
