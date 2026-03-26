a0a7c71255c8cfe9d486698fad51045aa2d201fe
GabrielSerra79 <bielserra@gmail.com>
2026-03-26 16:56:12 -0300

    Aula 660

---
+++ scripts_dbeaver/select_basico.sql
@@ -0,0 +1,13 @@
+use base_de_dados;
+
+SELECT * FROM users;
+
+SELECT email  FROM users;
+
+SELECT email, id  FROM users;
+
+SELECT email, id , first_name FROM users;
+
+SELECT email, id , first_name FROM users u;
+
+SELECT u.last_name FROM users u ;
