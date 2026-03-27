7f44b8277c9a35d17d26713dd255ade87e8de1a4
GabrielSerra79 <bielserra@gmail.com>
2026-03-27 17:45:04 -0300

    Aula 679

---
+++ sql/select_join.sql
@@ -0,0 +1,12 @@
+SELECT * FROM users u;
+
+SELECT * FROM profiles p;
+
+SELECT * FROM roles;
+
+SELECT u.id as uid, u.first_name, p.bio, r.name, ur.role_id
+FROM users as u
+    LEFT JOIN profiles as p ON u.id = p.user_id
+	INNER JOIN users_roles as ur ON u.id = ur.user_id
+	INNER JOIN roles as r ON ur.role_id = r.id
+	ORDER BY uid ASC;
